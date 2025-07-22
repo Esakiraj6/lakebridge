e0da48307d64297b1ca854b71014228e47fcfc75
sundar.shankar <sundar.shankar@databricks.com>
2025-07-22 10:14:29 +0530

    fix failing integration tests

--- .github/scripts/setup_mssql_odbc.sh
+++ .github/scripts/setup_mssql_odbc.sh
@@ -16,6 +16,9 @@ expected_hash=$(grep "${PKG_NAME}" "${FILE_MANIFEST}" | cut -d',' -f2)
 curl -sSL -O "https://packages.microsoft.com/config/ubuntu/${VERSION_ID}/${PKG_NAME}"
 printf "%s *packages-microsoft-prod.deb\n" "${expected_hash}" | sha256sum -c -
 
+# Install the Microsoft package repository configuration file using dpkg.
+# The `--force-confold` option ensures that existing configuration files are not overwritten
+# The `DEBIAN_FRONTEND=noninteractive` environment variable suppresses interactive prompts
 sudo DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i packages-microsoft-prod.deb
 #rm packages-microsoft-prod.deb
 
