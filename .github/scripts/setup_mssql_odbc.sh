284c5318737d8428def870e183c343d68b15ce6a
sundar.shankar <sundar.shankar@databricks.com>
2025-07-22 10:10:32 +0530

    fix failing integration tests

--- .github/scripts/setup_mssql_odbc.sh
+++ .github/scripts/setup_mssql_odbc.sh
@@ -16,7 +16,7 @@ expected_hash=$(grep "${PKG_NAME}" "${FILE_MANIFEST}" | cut -d',' -f2)
 curl -sSL -O "https://packages.microsoft.com/config/ubuntu/${VERSION_ID}/${PKG_NAME}"
 printf "%s *packages-microsoft-prod.deb\n" "${expected_hash}" | sha256sum -c -
 
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i packages-microsoft-prod.deb
+sudo DEBIAN_FRONTEND=noninteractive dpkg --force-confold -i packages-microsoft-prod.deb
 #rm packages-microsoft-prod.deb
 
 sudo apt-get update
